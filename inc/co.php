08b4b7dc8beaa0ca4fc0e88d6adef3f2eec15d7e
nyandooooo <s8niklaus@gmail.com>
2025-07-14 09:36:26 +0300

    f

---
+++ inc/co.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('display_errors', '1');
+function dbconnect()
+{
+    static $connect = null;
+
+    if ($connect === null) {
+        $connect = mysqli_connect('localhost', 'root', '', 'employees');
+
+        if (!$connect) {
+            die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
+        }
+    }
+
+    return $connect;
+}
